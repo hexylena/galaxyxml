59de6542ac973a996b12aab894899ec9996e2a0c
Eric Rasche <rasche.eric@yandex.ru>
2015-01-10 09:23:30 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="galaxyxml",
+        version='0.1',
+        description='Galaxy XML generation library',
+        author='Eric Rasche',
+        author_email='rasche.eric@yandex.ru',
+        license='GPL3',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Operating System :: OS Independent',
+            'Intended Audience :: Developers',
+            'Environment :: Console',
+            ],
+        )
