a40e1b7d3025db57c88bbe6caa2c4a5c6ff8d44b
Eric Rasche <rasche.eric@yandex.ru>
2015-01-10 09:44:42 -0600

    Added setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
+requirements = [x.strip() for x in open('requirements.txt', 'r').readlines()]
+
 setup(name="galaxyxml",
         version='0.1',
         description='Galaxy XML generation library',
         author='Eric Rasche',
         author_email='rasche.eric@yandex.ru',
         license='GPL3',
+        install_requires=requirements,
         classifiers=[
             'Development Status :: 3 - Alpha',
             'Operating System :: OS Independent',
