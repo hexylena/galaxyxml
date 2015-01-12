a1d400e057f00c2906a97c2b584eee02c13bb4bf
Eric Rasche <rasche.eric@yandex.ru>
2015-01-12 16:27:34 -0600

    I give up...

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
-
-requirements = [x.strip() for x in open('requirements.txt', 'r').readlines()]
+from pip.req import parse_requirements
 
 setup(name="galaxyxml",
-        version='0.1',
+        version='0.1.1',
         description='Galaxy XML generation library',
         author='Eric Rasche',
         author_email='rasche.eric@yandex.ru',
         license='GPL3',
-        install_requires=requirements,
+        install_requires=['lxml'],
         packages=["galaxyxml", "galaxyxml.tool", "galaxyxml.tool.parameters"],
         classifiers=[
             'Development Status :: 3 - Alpha',
