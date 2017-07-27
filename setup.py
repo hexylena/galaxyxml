736d3e3b8e6ad44ac97cc6e430cd0aa82407082c
khillion <kenzo-hugo.hillion1@pasteur.fr>
2017-07-27 09:02:14 +0200

    modify version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="galaxyxml",
-        version='0.4.0',
+        version='0.4.1',
         description='Galaxy XML generation library',
         author='Eric Rasche',
         author_email='esr@tamu.edu',
