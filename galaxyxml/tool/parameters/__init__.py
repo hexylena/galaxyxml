da7338808a28a99bb5b7f92030ba6207754fd663
fubar2 <ross.lazarus@gmail.com>
2020-07-30 16:07:03 +1000

    added spaces to textparam command line in case of spaces

--- galaxyxml/tool/parameters/__init__.py
+++ galaxyxml/tool/parameters/__init__.py
@@ -379,6 +379,15 @@ class TextParam(Param):
         params = Util.clean_kwargs(locals().copy())
         super(TextParam, self).__init__(**params)
 
+    def command_line_actual(self): # quote in case of spaces
+        try:
+            return self.command_line_override
+        except Exception:
+            if self.positional:
+                return self.mako_name()
+            else:
+                return '%s%s"%s"' % (self.flag(), self.space_between_arg, self.mako_name())
+
 
 class _NumericParam(Param):
 
