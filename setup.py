cf3e7124577fbeb6888b3d3502ed069f7b7d9714
Helena Rasche <hexylena@galaxians.org>
2023-09-22 09:56:36 +0200

    screaming, crying

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as fh:
 
 setup(
     name="galaxyxml",
-    version="0.5.2",
+    version="0.5.3",
     description="Galaxy XML generation library",
     author="Helena Rasche",
     author_email="hexylena@galaxians.org",
